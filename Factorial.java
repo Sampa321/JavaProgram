42d486f2fc98b690027c95b333b09c7842ec543d
Sampa Nayak <sampanayak987@gmail.com>
2025-08-06 00:10:19 +0530

    Add files via upload
    Write a java recursive method to calculate the factorial of a given positive integer.

--- Factorial.java
+++ Factorial.java
@@ -1,18 +1,34 @@
-package HomePractice.SomeQuestion;
-/*Factorial of a number*/
+package HomePractice.W3resource;
+
+/*Write a java recursive method to calculate the factorial of a given positive integer. */
 
 import java.util.Scanner;
+
 public class Factorial {
-    public static void main(String[] args)
-    {
-        Scanner SC=new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number:");
-        int a= SC.nextInt();
-        int fact=1;
-        for(int i=1;i<=a;i++)
+        int n = sc.nextInt();
+        if(n>=0)
         {
-            fact*=i;
+            int r = calculateFactorial(n);
+            System.out.println("Factorial of "+n+" : "+r);
         }
-        System.out.println("Factorial of a number : "+fact);
+        else {
+            System.out.println("Factorial calculation can't possible!");
+        }
     }
+
+    public static int calculateFactorial(int n)
+    {
+        if(n==0 || n==1)
+        {
+            return 1;
+        }
+        else {
+            return n*calculateFactorial(n-1);
+        }
+    }
+
 }
+
